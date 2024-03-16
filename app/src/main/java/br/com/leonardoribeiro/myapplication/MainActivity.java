0daccc3307cce22b0a2944912ae2e6ff6859d7ab
leonardo <leo.alcantara.lr@gmail.com>
2024-03-16 17:31:34 -0400

    atividade avaliativa

--- app/src/main/java/br/com/leonardoribeiro/myapplication/MainActivity.java
+++ app/src/main/java/br/com/leonardoribeiro/myapplication/MainActivity.java
@@ -1,24 +1,38 @@
-package br.com.leonardoribeiro.myapplication;
+package br.com.app1.aule;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText campoNome;
+    Button btnSaudacao;
+    TextView tvMensagem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        campoNome = findViewById(R.id.editNome);
+        btnSaudacao = findViewById(R.id.btnBotao);
+        tvMensagem = findViewById(R.id.tvNome);
+
+        btnSaudacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //PEGAR A INFORMAÇÃO DO EDITTEXT
+                String nome = campoNome.getText().toString();
+                String mensagem = "Olá" + nome;
+                tvMensagem.setText(mensagem);
+
+            }
         });
     }
 }
